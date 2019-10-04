c0db1b3c4ebb616fd6a11f0bfd9a178becc55fbf
Haris <learn@learntpoint.com>
2019-10-04 12:34:58 +0200

    finished

--- assets/main.js
+++ assets/main.js
@@ -1,38 +1,33 @@
-// SELECTORS
 const calcBtn = document.getElementById('calcBtn')
 let priceOutput = document.getElementById('priceOutput')
 
-// LISTENERS
 calcBtn.addEventListener('click', (e) => {
     calculateInsurance()
     e.preventDefault()
 })
 
-// FUNCTIONS
 function calculateInsurance() {
-    // SELECTING INPUT VALUES
     let age = document.getElementById('age').value
     let hp = document.getElementById('hp').value
     let country = document.getElementById('country').value
 
     let insurance
 
-    // CHECKING THE COUNTRY INPUT VALUE WITH A SWITCH
     switch(country) {
 
         case "austria":
             insurance = (hp*100/age + 50)
-            priceOutput.textContent = `You have to pay ${insurance.toFixed(2)} euros.`
+            priceOutput.textContent = `You have to pay ${insurance.toFixed(2)} €`
             break
         
         case "hungary":
             insurance = (hp*120/age + 100)
-            priceOutput.textContent = `You have to pay ${insurance.toFixed(2)} euros.`
+            priceOutput.textContent = `You have to pay ${insurance.toFixed(2)} €`
             break
 
         case "greece":
             insurance = (hp*150/(age+3) + 50)
-            priceOutput.textContent = `You have to pay ${insurance.toFixed(2)} euros.`
+            priceOutput.textContent = `You have to pay ${insurance.toFixed(2)} €`
             break
 
     }
